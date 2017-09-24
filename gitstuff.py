4b1d453fb9727686372e092aa0935bd4f41f05de
Michael Chung <mchung96@stanford.edu>
2017-09-24 15:35:44 -0700

    added a bit of error checking for gitstuff.py

--- gitstuff.py
+++ gitstuff.py
@@ -4,7 +4,7 @@ from subprocess import call
 """
 USAGE:
 
-	python gitstuff.py <acp> "commit message"
+	python gitstuff.py -acp "<commit message>"
 
 * a runs 'git add .'
 * c runs 'git commit -m <commit message here>'
@@ -12,17 +12,22 @@ USAGE:
 * p runs 'git push'
 """
 def main(argv):
-	params = argv[1]
-	if 'a' in params:
-		call(['git', 'add', '.'])
-	if 'c' in params:
-		if len(argv) > 2:
-			message = argv[2]
-			call(['git', 'commit', '-m', message])
-		else:
-			print('Please include a commit message as a second parameter.')
-	if 'p' in params:
-		call(['git', 'push'])
+	if len(argv) <= 2:
+		print('Please choose one or more of the add, commit, and push options.')
+		print('Usage:\n\tgitstuff -acp "<commit message>"')
+	else:
+		params = argv[1]
+		if 'a' in params:
+			call(['git', 'add', '.'])
+		if 'c' in params:
+			if len(argv) > 2:
+				message = argv[2]
+				call(['git', 'commit', '-m', message])
+			else:
+				print('Please include a commit message as a second parameter.')
+				print('Usage: gitstuff -acp "<commit message>"')
+		if 'p' in params:
+			call(['git', 'push'])
 
 if __name__ == '__main__':
 	main(sys.argv)
