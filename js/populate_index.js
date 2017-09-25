69ad667cc05d77f68aa69081d1e24432dab7d048
Michael Chung <mchung96@stanford.edu>
2017-09-24 19:36:31 -0700

    oh nooooo

--- js/populate_index.js
+++ js/populate_index.js
@@ -7,10 +7,10 @@
 const NUM_CONCERTS = 10;
 
 function addConcerts() {
+	console.log('hello');
 	$.getJSON('res/concerts.json', function(data) {
 		let concerts = data.concerts;
 		console.log(concerts);
-		console.log('hello');
 		const concertSlider = document.querySelector('#concert-slider');
 		for (let i = 0; i < NUM_CONCERTS; i++) {
 			if (i < concerts.length) {
